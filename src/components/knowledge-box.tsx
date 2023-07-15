81c09085b88b2f6a365434ced72dede01d4871ac
msp5382 <meen.sankaew@gmail.com>
2023-07-16 03:09:43 +0700

    feat: structure

---
+++ src/components/knowledge-box.tsx
@@ -0,0 +1,16 @@
+export const KnowledgeBox = () => {
+
+  return (<div className="py-7 overflow-hidden">
+    <div className="w-full shadow-shadow-primary shadow-x-none px-6 py-5 overflow-hidden">
+      <h1 className="font-bold text-xl">ความรู้</h1>
+      <p className="text-sm">
+        Lorem Ipsum is simply dummy text of the.Lorem Ipsum is simply dummy text of the Lorem Ipsum is simply dummy text of the Lorem Ipsum is simply dummy text of the Lorem Ipsum is simply dummy text of the Lorem Ipsum is simply dummy text of the Lorem Ipsum is simply dummy text of the Lorem Ipsum is simply dummy text of the Lorem Ipsum is simply dummy text of the Lorem Ipsum is simply dummy text of the
+      </p>
+      <div className="flex justify-end mt-4">
+        <button className="rounded-lg bg-primary shadow-shadow-primary text-sm px-3 py-1 text-white">อ่านเพิ่มเติม</button>
+      </div>
+
+    </div>
+  </div>)
+
+}
