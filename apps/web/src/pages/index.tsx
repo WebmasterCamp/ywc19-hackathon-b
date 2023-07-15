1626840d7dfb918a6caba0780f616dfe14dc6efc
msp5382 <meen.sankaew@gmail.com>
2023-07-15 21:36:16 +0700

    feat: color

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <div className="flex custom-gradient-primary min-h-screen w-screen mx-auto max-w-[calc(9/18.5*100vh)]">
-      <div className='m-auto text-5xl text-center font-bold'>Hello Bright Planet</div>
+      <div className='m-auto text-5xl text-center font-bold text-white'>Hello Bright Planet</div>
     </div>
   )
 }
