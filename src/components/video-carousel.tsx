4b6ddf7702695dc0bc1b2e075ce81bf54d888823
msp5382 <meen.sankaew@gmail.com>
2023-07-16 02:47:04 +0700

    feat: video carousel

--- src/components/video-carousel.tsx
+++ src/components/video-carousel.tsx
@@ -1,8 +1,8 @@
 import { useSpringCarousel } from "react-spring-carousel";
 
-const VideoItem = () => <div className="pr-5">
-  <div className="bg-[#BECFE9] w-[141px] h-[79px]"></div>
-  <p className="text-sm">Lorem Ipsum is simply dummy text of the</p>
+const VideoItem = () => <div className="pr-5 text-primary font-medium">
+  <div className="bg-primary w-[141px] h-[79px]"></div>
+  <p className="text-sm">ทดสอบห</p>
 </div>
 
 export const VideoCarousel = () => {
@@ -16,5 +16,12 @@ export const VideoCarousel = () => {
       )
     }))
   })
-  return <div className="w-full overflow-hidden">{carouselFragment}</div>
+  return (<div className="py-7 overflow-hidden">
+    <div className="w-full shadow-shadow-primary shadow-x-none px-6 py-5 overflow-hidden">
+      <h1 className="font-bold text-xl">ท่าสำหรับยืดตัว</h1>
+      <h2 className="text-sm font-medium mb-4 text-secondary">ยืดๆ ยาดๆ บิดนิดนึง</h2>
+      {carouselFragment}
+    </div>
+  </div>)
+
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,8 @@ module.exports = {
     extend: {
       colors: {
         "primary": '#3E3E6B',
-        "primary-mid-emp": "rgba(125, 158, 211, 0.30)"
+        "primary-mid-emp": "rgba(125, 158, 211, 0.75)",
+        "secondary": "rgba(62, 62, 107, 0.75)"
       },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
@@ -17,6 +18,9 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       dropShadow: {
+        'shadow-primary': "0px 0px 34px 0px rgba(125, 158, 211, 0.50)"
+      },
+      boxShadow: {
         'shadow-primary': "0px 0px 34px 0px rgba(125, 158, 211, 0.50)"
       }
     },
